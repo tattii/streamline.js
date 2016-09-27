e9b9b53bfe08f0ca8abd40ec1e822c97157ff6e1
tattii <yuta.tatti@gmail.com>
2016-09-27 19:02:53 +0900

    update streamline.js from windmap

--- streamline.js
+++ streamline.js
@@ -1,5 +1,5 @@
 /**
- *   Streamline.js - vector field visualization using canvas
+ *   Streamline - vector field visualization using canvas
  *
  *   NOTE: stream.setField(field, projection);
  *		field: require getVector method
@@ -7,17 +7,21 @@
  *		projection: require unproject method
  *			project canvas point(x,y) to field point(cf. latlng)
  */
-function Streamline(bound) {
+function Streamline(bound, streamCtx, maskCtx) {
 
 	var PARTICLE_MULTIPLIER = 7;
 	var PARTICLE_LINE_WIDTH = 1.0;
 	var MAX_PARTICLE_AGE = 100;
+	var MASK_ALPHA = Math.floor(0.3*255);
 	var FRAME_RATE = 40;
 	var NULL_VECTOR = [NaN, NaN, null];
+	var TRANSPARENT_BLACK = [0,0,0,0];
 
 	bound.width  = bound.x[1] - bound.x[0];
 	bound.height = bound.y[1] - bound.y[0];
 
+	var timer;  // frame rate timer
+	var mask = Mask();
 	
 	/**
 	 *	Grid - canvasと同じ大きさのベクトル集合
@@ -28,7 +32,8 @@ function Streamline(bound) {
 	var Grid = function(){
 		var rows = [];
 
-		function set(field, projection) {
+		function set(field, projection, scale) {
+			if ( !scale ) scale = 1;
 			rows = [];
 			for (var y = bound.y[0]; y < bound.y[1]; y+=2){
 				interpolateRow(y);
@@ -39,8 +44,14 @@ function Streamline(bound) {
 				for (var x = bound.x[0]; x < bound.x[1]; x+=2){
 					var latlng = projection.unproject(x, y);
 					var v = field.getVector(latlng);
-					var wind = [ v[0], v[1], Math.sqrt(v[0]*v[0] + v[1]*v[1]) ];
+					var wind = (v[0] == null) ? NULL_VECTOR : [ v[0]*scale, v[1]*-1*scale, Math.sqrt(v[0]*v[0] + v[1]*v[1]) ];
 					row[x] = row[x+1] = wind;
+
+					var color = (v[0] == null) ? TRANSPARENT_BLACK : extendedSinebowColor(Math.min(wind[2], 100) / 100, MASK_ALPHA);
+					mask.set(x,   y,   color)
+						.set(x+1, y,   color)
+						.set(x,   y+1, color)
+						.set(x+1, y+1, color);
 				}
 				rows[y] = rows[y+1] = row;
 			}
@@ -81,7 +92,61 @@ function Streamline(bound) {
 	}();
 
 
+	function Mask(){
+		maskCtx.fillStyle = "rgba(255,0,0,1)";
+		maskCtx.fill();
+
+		var imageData = maskCtx.getImageData(0, 0, bound.width, bound.height);
+		var data = imageData.data;
+
+		return {
+			imageData: imageData,
+			isVisible: function(x, y) {
+				return data[(y*bound.width + x)*4 + 3] > 0;
+			},
+			set: function(x, y, rgba) {
+				var i = (y * bound.width + x) * 4;
+				data[ i ] = rgba[0];
+				data[i+1] = rgba[1];
+				data[i+2] = rgba[2];
+				data[i+3] = rgba[3];
+				return this;
+			}
+		};
+	}
+
 	
+	// colors
+	function colorInterpolator(start, end) {
+		var r = start[0], g = start[1], b = start[2];
+		var dr = end[0] - r, dg = end[1] - g, db = end[2] - b;
+		return function(i, a) {
+			return [Math.floor(r + i * dr), Math.floor(g + i * dg), Math.floor(b + i * db), a];
+		};
+	}
+
+	function sinebowColor(hue, a) {
+		// Map hue [0, 1] to radians [0, 5/6τ]. Don't allow a full rotation because that keeps hue == 0 and
+		// hue == 1 from mapping to the same color.
+		var rad = hue * 2 * Math.PI * 5/6;
+		rad *= 0.75;  // increase frequency to 2/3 cycle per rad
+
+		var s = Math.sin(rad);
+		var c = Math.cos(rad);
+		var r = Math.floor(Math.max(0, -c) * 255);
+		var g = Math.floor(Math.max(s, 0) * 255);
+		var b = Math.floor(Math.max(c, 0, -s) * 255);
+		return [r, g, b, a];
+	}
+
+	var BOUNDARY = 0.45;
+	var fadeToWhite = colorInterpolator(sinebowColor(1.0, 0), [255, 255, 255]);
+	function extendedSinebowColor(i, a) {
+		return i <= BOUNDARY ?
+			sinebowColor(i / BOUNDARY, a) :
+			fadeToWhite((i - BOUNDARY) / (1 - BOUNDARY), a);
+	}
+
 	function colorScale(step, maxWind){
 		function asColorStyle(r, g, b, a) {
 			return "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
@@ -98,15 +163,26 @@ function Streamline(bound) {
 	}
 
 
+	function setField(field, projection, scale){
+		if (timer) clearTimeout(timer);
+		Grid.release();
+		Grid.set(field, projection, scale);
+	}
+
+
+
+
 	/**
 	 *   animate stream
 	 *		require canvas context
 	 */
-	function animate(ctx){
+	function animate(density){
 		var color = colorScale(10, 17);
 		var fadeFillStyle = "rgba(0, 0, 0, 0.97)";
-		var buckets = color.map(function(){ return []; });i
-		var particleCount = Math.round(bound.width * PARTICLE_MULTIPLIER);
+		var buckets = color.map(function(){ return []; });
+		if ( !density ) density = 1;
+		var particleCount = Math.round(bound.width * PARTICLE_MULTIPLIER * density);
+		console.log("particles:" + particleCount)
 		var particles = [];
 		for (var i = 0; i < particleCount; i++) {
 			particles.push(Grid.randomize({age: _.random(0, MAX_PARTICLE_AGE)}));
@@ -143,38 +219,39 @@ function Streamline(bound) {
 			});
 		}
 
-		ctx.lineWidth = PARTICLE_LINE_WIDTH;
-		ctx.fillStyle = fadeFillStyle;
+		streamCtx.lineWidth = PARTICLE_LINE_WIDTH;
+		streamCtx.fillStyle = fadeFillStyle;
 
 		function draw() {
 			function fade(){
-            	var prev = ctx.globalCompositeOperation;
-            	ctx.globalCompositeOperation = "destination-in";
-            	ctx.fillRect(bound.x[0], bound.y[0], bound.width, bound.height);
-            	ctx.globalCompositeOperation = prev;
+            	var prev = streamCtx.globalCompositeOperation;
+            	streamCtx.globalCompositeOperation = "destination-in";
+            	streamCtx.fillRect(bound.x[0], bound.y[0], bound.width, bound.height);
+            	streamCtx.globalCompositeOperation = prev;
 			}
 
 			fade();
 			buckets.forEach(function(bucket, i) {
 				if ( bucket.length > 0 ){
-					ctx.beginPath();
-					ctx.strokeStyle = color[i];
+					streamCtx.beginPath();
+					streamCtx.strokeStyle = color[i];
 					bucket.forEach(function(particle) {
-						ctx.moveTo(particle.x, particle.y);
-						ctx.lineTo(particle.xt, particle.yt);
+						streamCtx.moveTo(particle.x, particle.y);
+						streamCtx.lineTo(particle.xt, particle.yt);
 						particle.x = particle.xt;
 						particle.y = particle.yt;
 					});
-					ctx.stroke();
+					streamCtx.stroke();
 				}
 			});
 		}
 
+		maskCtx.putImageData(mask.imageData, 0, 0);
 		(function frame() {
 			try {
 				evolve();
 				draw();
-				setTimeout(frame, FRAME_RATE);
+				timer = setTimeout(frame, FRAME_RATE);
 
 			} catch(e) {
 				console.log(e);
@@ -183,7 +260,7 @@ function Streamline(bound) {
 	}
 
 	return {
-		setField: function(f,p){ Grid.set(f,p); },
+		setField: setField,
 		animate: animate
 	};
 }
